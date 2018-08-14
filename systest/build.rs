e6ef764fd8dfacb0fd986be218d44b45e9d92ff8
Alex Crichton <alex@alexcrichton.com>
2018-08-13 17:06:24 -0700

    Bump ctest to 0.2

--- systest/build.rs
+++ systest/build.rs
@@ -7,9 +7,9 @@ fn main() {
     if let Ok(out) = env::var("DEP_LZMA_INCLUDE") {
         cfg.include(&out);
     }
-    
+
     cfg.header("lzma.h");
-    cfg.type_name(|n, _s| n.to_string());
+    cfg.type_name(|n, _s, _| n.to_string());
     cfg.define("LZMA_API_STATIC", None);
     cfg.skip_type(|n| n == "__enum_ty");
     cfg.generate("../lzma-sys/src/lib.rs", "all.rs");
