341e297b1c4abb065b439fdd2e8841c5dd30ffc6
Alex Crichton <alex@alexcrichton.com>
2019-08-23 10:47:45 -0700

    Run rustfmt

--- src/write.rs
+++ src/write.rs
@@ -7,7 +7,7 @@ use std::io::prelude::*;
 #[cfg(feature = "tokio")]
 use futures::Poll;
 #[cfg(feature = "tokio")]
-use tokio_io::{AsyncRead, AsyncWrite, try_nb};
+use tokio_io::{try_nb, AsyncRead, AsyncWrite};
 
 use crate::stream::{Action, Check, Status, Stream};
 
